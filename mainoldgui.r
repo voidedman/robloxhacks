e09c46556c09a2847596bcd9bc1be7d164a79db7
voidedman <92179972+voidedman@users.noreply.github.com>
2021-10-30 15:05:35 -0400

    Create mainoldgui.r

---
+++ mainoldgui.r
@@ -0,0 +1,397 @@
+-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)-- Made by SECRET_INFO99
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Label = Instance.new("TextLabel")
+local EclipseGUIButton = Instance.new("TextButton")
+local UpdateLabel = Instance.new("TextLabel")
+local VyunixuGUIButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(53, 53, 53)
+Main.Position = UDim2.new(0.541088581, 0, 0.561128497, 0)
+Main.Size = UDim2.new(0, 400, 0, 250)
+Main.Active = true
+Main.Draggable = true
+
+Label.Name = "Label"
+Label.Parent = Main
+Label.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+Label.Size = UDim2.new(0, 400, 0, 29)
+Label.Font = Enum.Font.Ubuntu
+Label.Text = "MM2 Script Hub - v1 | Made by SECRET_INFO99"
+Label.TextColor3 = Color3.fromRGB(0, 0, 0)
+Label.TextSize = 14.000
+
+EclipseGUIButton.Name = "EclipseGUIButton"
+EclipseGUIButton.Parent = Main
+EclipseGUIButton.BackgroundColor3 = Color3.fromRGB(52, 222, 255)
+EclipseGUIButton.Position = UDim2.new(0.25, 0, 0.179999992, 0)
+EclipseGUIButton.Size = UDim2.new(0, 200, 0, 50)
+EclipseGUIButton.Font = Enum.Font.Oswald
+EclipseGUIButton.Text = "Eclipse GUI"
+EclipseGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+EclipseGUIButton.TextSize = 50.000
+EclipseGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+end)
+
+UpdateLabel.Name = "UpdateLabel"
+UpdateLabel.Parent = Main
+UpdateLabel.BackgroundColor3 = Color3.fromRGB(98, 98, 98)
+UpdateLabel.Position = UDim2.new(0, 0, 0.800000131, 0)
+UpdateLabel.Size = UDim2.new(0, 400, 0, 50)
+UpdateLabel.Font = Enum.Font.SourceSans
+UpdateLabel.Text = "UPDATE SOON - Check this GUI's Pastebin or the RBXploits web page for more"
+UpdateLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+UpdateLabel.TextSize = 14.000
+
+VyunixuGUIButton.Name = "VyunixuGUIButton"
+VyunixuGUIButton.Parent = Main
+VyunixuGUIButton.BackgroundColor3 = Color3.fromRGB(178, 142, 255)
+VyunixuGUIButton.Position = UDim2.new(0.25, 0, 0.460000008, 0)
+VyunixuGUIButton.Size = UDim2.new(0, 200, 0, 50)
+VyunixuGUIButton.Font = Enum.Font.Oswald
+VyunixuGUIButton.Text = "Vyunixu GUI"
+VyunixuGUIButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+VyunixuGUIButton.TextSize = 50.000
+VyunixuGUIButton.MouseButton1Down:connect(function()
+	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+end)
